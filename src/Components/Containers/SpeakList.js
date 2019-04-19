302050b23d3b557f760d79a04e61ed88f63ae4c3
adambodie <adam.bodie@yahoo.com>
2019-04-18 20:58:46 -0700

    Updated Speak, Buy component styles

---
+++ src/Components/Containers/SpeakList.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+export default class SpeakList extends Component {
+	  constructor() {
+		super();
+			this.state = {
+				value: '',
+				text: ''
+			};
+			this.handleSubmit = this.handleSubmit.bind(this);
+			this.handleChange = this.handleChange.bind(this);
+			
+		}
+		handleSubmit(e) {
+			const { value, text } = this.state;
+			if (value === 'Delete') {
+				const length = text.length;
+				this.setState({text: text.substr(0,length - 1)});
+			} else if (value === 'Clear'){
+				this.setState({text: ""});
+			} else if (value === 'Space'){
+				this.setState({text: text + " "});
+			} else {
+				this.setState({text: text + value});
+		}
+			e.preventDefault();
+		}
+		handleChange(e) {
+			this.setState({value: e.target.value});
+		}
+	render() {
+		const { buttons } = this.props;
+		const { text } = this.state;
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<h1 className='keyboard-enter'>{text}</h1>
+				<div className="keys">
+					{buttons.map((x, index) => <input onClick={this.handleChange} type='submit' value={x} key={index}/>)}
+				</div>
+			</form>
+		)
+	}
+}
+
+
